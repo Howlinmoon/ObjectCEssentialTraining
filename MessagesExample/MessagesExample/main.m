0771726702119f427e09eca5bfac9c2658ed225d
Jim Veneskey <github@strifecrafter.com>
2015-07-14 12:33:40 -0400

    messaging and if example in one

--- MessagesExample/MessagesExample/main.m
+++ MessagesExample/MessagesExample/main.m
@@ -8,14 +8,34 @@
 
 #import <Foundation/Foundation.h>
 
-int main(int argc, const char * argv[])
-{
+int main(int argc, const char * argv[]) {
 
     @autoreleasepool {
         
-        // insert code here...
-        NSLog(@"Hello, World!");
+        int minutes = 60;
+        int hours = 24;
+        int days = 365;
         
+        int minutesInAYear = minutes * hours * days;
+        
+        NSLog(@"There are %i minutes in a year", minutesInAYear);
+        
+        
+        // If example
+        
+        
+        int stormCategory = 4;
+        
+        if (stormCategory > 1) {
+            NSLog(@"Storm's a-coming!");
+        }
+        
+        if (stormCategory == 4 || stormCategory == 5) {
+            NSLog(@"Catastrophe!");
+        }
+        
+        
+
     }
     return 0;
 }
