0c3846b7359c18601851259c3cbc5a8b4706740e
Jim Veneskey <github@strifecrafter.com>
2015-07-15 13:02:06 -0400

    Experimenting with floats and doubles

--- NumericTypes/NumericTypes/main.m
+++ NumericTypes/NumericTypes/main.m
@@ -26,6 +26,33 @@ int main(int argc, const char * argv[])
         
         NSLog(@"My unsigned integers are %u and %u", myUnsignedInteger, myOtherUnsignedInteger);
         
+        // ints and truncation
+        int nearlyFive = 4.999999;
+        NSLog(@"The value of nearlyFive is: %i", nearlyFive);
+        
+        // and during calculations
+        int myResult = 10 / 4;
+        NSLog(@"The result of my division is: %i", myResult);
+        
+        
+        // Defining a float
+        float myFloat = 33.3f;
+        NSLog(@"myFloat using percent f is: %f", myFloat);
+        // Scientific notation
+        NSLog(@"myFloat using percent e is: %e", myFloat);
+        // best guess
+        NSLog(@"myFloat using percent g is: %g", myFloat);
+        
+        
+        // Defining a double
+        double myDouble = 33.3;
+        NSLog(@"myDouble using percent f is: %f", myDouble);
+        // Scientific notation
+        NSLog(@"myDouble using percent e is: %e", myDouble);
+        // best guess
+        NSLog(@"myDouble using percent g is: %g", myDouble);
+        
+        
     }
     return 0;
 }
