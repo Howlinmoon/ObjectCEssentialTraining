c93aad8f7207fe38a24b79e09535b03084cd1548
Jim Veneskey <github@strifecrafter.com>
2015-07-15 12:30:49 -0400

    Reverting to allow xcode to do the work for us in the next step

--- FunctionStarter/FunctionStarter/main.m
+++ FunctionStarter/FunctionStarter/main.m
@@ -8,28 +8,19 @@
 
 #import <Foundation/Foundation.h>
 
-// Old style method - 1999, using a function prototype to tell the compiler
-// about a function that is not yet defined
-void myFunction();
-
 int main(int argc, const char * argv[]) {
 
     @autoreleasepool {
-        myFunction();
+        // a simple loop
+        for (int i = 1; i < 5000; i++) {
+            if (i % 5 == 0) {
+                continue; // skip multiples of 5
+            }
+            NSLog(@"The value of the index is %i", i);
+        }
         
     }
     return 0;
-}
-
-// Here is where we define the function - AFTER we have called it
-void myFunction () {
-    // a simple loop
-    for (int i = 1; i < 5000; i++) {
-        if (i % 5 == 0) {
-            continue; // skip multiples of 5
-        }
-        NSLog(@"The value of the index is %i", i);
-    }
 }
 
 
