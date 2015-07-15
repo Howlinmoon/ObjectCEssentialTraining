3a8c821e3e10b76044d96b625af58f4c97d18f65
Jim Veneskey <github@strifecrafter.com>
2015-07-15 12:28:03 -0400

    Experimenting with prototypes for functions - when the function definition is AFTER the code that calls it

--- FunctionStarter/FunctionStarter/main.m
+++ FunctionStarter/FunctionStarter/main.m
@@ -8,20 +8,28 @@
 
 #import <Foundation/Foundation.h>
 
-int main(int argc, const char * argv[])
-{
+// Old style method - 1999, using a function prototype to tell the compiler
+// about a function that is not yet defined
+void myFunction();
+
+int main(int argc, const char * argv[]) {
 
     @autoreleasepool {
-        
-      // a simple loop
-        for (int i = 1; i < 5000; i++) {
-            if (i % 5 == 0) {
-                continue; // skip multiples of 5
-            }
-            NSLog(@"The value of the index is %i", i);
-        }
+        myFunction();
         
     }
     return 0;
 }
+
+// Here is where we define the function - AFTER we have called it
+void myFunction () {
+    // a simple loop
+    for (int i = 1; i < 5000; i++) {
+        if (i % 5 == 0) {
+            continue; // skip multiples of 5
+        }
+        NSLog(@"The value of the index is %i", i);
+    }
+}
+
 
