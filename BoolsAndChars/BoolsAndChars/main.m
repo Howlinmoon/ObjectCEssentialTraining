00c7d0ce44382efdc79f87a88f4fce144b305001
Jim Veneskey <github@strifecrafter.com>
2015-07-16 12:13:01 -0400

    Continuing along, this time with Bools and Characters

---
+++ BoolsAndChars/BoolsAndChars/main.m
@@ -0,0 +1,21 @@
+//
+//  main.m
+//  BoolsAndChars
+//
+//  Created by jim Veneskey on 7/16/15.
+//  Copyright (c) 2015 Jim Veneskey. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+
+        
+    }
+    return 0;
+}
+
