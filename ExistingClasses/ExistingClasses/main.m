70a0c1d4bafe5049994664380d002620b8f3df8b
Jim Veneskey <github@strifecrafter.com>
2015-07-17 12:21:23 -0400

    Experimenting with calling pre-existing classes

---
+++ ExistingClasses/ExistingClasses/main.m
@@ -0,0 +1,26 @@
+//
+//  main.m
+//  ExistingClasses
+//
+//  Created by jim Veneskey on 7/17/15.
+//  Copyright (c) 2015 Jim Veneskey. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+
+        NSString *message = @"Hello";
+        NSString *shoutyMessage = [message uppercaseString];
+        
+        // did anything happen?
+        NSLog(@"The NSString objects are %@ and %@", message, shoutyMessage);
+
+        
+    }
+    return 0;
+}
+
