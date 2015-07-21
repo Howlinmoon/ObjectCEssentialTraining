81889f3c6be9700735c12416216b313c98459c39
Jim Veneskey <github@strifecrafter.com>
2015-07-21 13:01:55 -0400

    More experimenting with the file manager

---
+++ DirectoryExample/DirectoryExample/main.m
@@ -0,0 +1,41 @@
+//
+//  main.m
+//  DirectoryExample
+//
+//  Created by jim Veneskey on 7/21/15.
+//  Copyright (c) 2015 Jim Veneskey. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+
+        
+        
+        // Simple function call to determine the users home directory
+        NSString *homeDirectory = NSHomeDirectory();
+        NSLog(@"Calling NSHomeDirectory() results in: %@", homeDirectory);
+        NSString *desktopPath = [homeDirectory stringByAppendingPathComponent:@"Desktop"];
+        NSLog(@"Path is now: %@", desktopPath);
+        
+        
+        
+        
+        
+        // More complex and powerful - note, not alloc or init this time
+        // this creates a shared one
+        NSFileManager *fileManager = [NSFileManager defaultManager];
+        
+        NSURL *docsDirectory = [fileManager URLForDirectory:NSDocumentDirectory inDomain:NSUserDomainMask appropriateForURL:nil create:NO error:nil];
+        
+        NSLog(@"File Manager returned: %@ ", docsDirectory);
+        
+        
+    }
+    return 0;
+}
+
