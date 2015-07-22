aaf284c7ba655a44f6b500873d1395f83edf1797
Jim Veneskey <github@strifecrafter.com>
2015-07-22 12:54:19 -0400

    reading and now writing files

--- ReadWriteStrings/ReadWriteStrings/main.m
+++ ReadWriteStrings/ReadWriteStrings/main.m
@@ -19,13 +19,23 @@ int main(int argc, const char * argv[])
         // using the documents directory object - point to the file sample.txt
         NSURL *full = [documentDir URLByAppendingPathComponent:@"sample.txt"];
         
-        // create a string for reading in the file
-        NSString *content = [[NSString alloc] initWithContentsOfURL:full encoding:NSUTF8StringEncoding error:nil];
+        // create a string for reading in the file (now mutable)
+        NSMutableString *content = [[NSMutableString alloc] initWithContentsOfURL:full encoding:NSUTF8StringEncoding error:nil];
         
         // write it out
-        NSLog(@"The string is: %@", content);
+        NSLog(@"The original content is: %@", content);
         
+        // Update the string
+        [content appendString:@"This is new content added to the string"];
+
+        // write it out
+        NSLog(@"The updated content is: %@", content);
         
+        // Create a new filename to store it under
+        NSURL *saveLocation = [documentDir URLByAppendingPathComponent:@"saved.text"];
+        
+        // Write it out using a method of the NSURL
+        [content writeToURL:saveLocation atomically:YES encoding:NSUTF8StringEncoding error:nil];
         
         
     }
